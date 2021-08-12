fdd25fe5a5dd63aca94de54355529a89d563f150
prasadandroiddeveloper@gmail.com <sbhaskar.sreddy@gmail.com>
2021-08-12 13:39:37 +0530

    Updated Tfvars

--- variable.tf
+++ variable.tf
@@ -4,9 +4,12 @@ variable "ssh-location" {
   type        = string
 }
 
-
-
-
+variable "aws_access_key"{
+}
+variable "aws_secret_key" {
+}
+variable "region" {
+}
 variable "vpc-cidr" {
   default = "10.0.0.0/16"
   type    = string
