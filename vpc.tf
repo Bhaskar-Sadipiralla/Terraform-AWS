f1b782c2de74a24570d6786ba523752af86abaa5
prasadandroiddeveloper@gmail.com <sbhaskar.sreddy@gmail.com>
2021-08-12 18:21:10 +0530

    Updated vpc, main, dev.tfavrs files

--- vpc.tf
+++ vpc.tf
@@ -4,7 +4,7 @@ resource "aws_vpc" "terraform-vpc" {
   enable_dns_hostnames = true
 
   tags = {
-    Name = "Terraform VPC"
+    Name = "${var.envname}"
   }
 }
 
@@ -14,37 +14,69 @@ resource "aws_internet_gateway" "internet-gateway" {
   vpc_id = aws_vpc.terraform-vpc.id
 
   tags = {
-    Name = "Terraform IGW"
+    Name = "${var.envname}-IGW"
   }
 }
 
-# Create Public Subnet 1
-# terraform aws create subnet
-resource "aws_subnet" "public-subnet-1" {
+# Create Public Subnet 
+#Public Subnets using terraform
+resource "aws_subnet" "public-subnets" {
+  count = length(var.azs)
   vpc_id                  = aws_vpc.terraform-vpc.id
-  cidr_block              = var.public-subnet-1-cidr
-  availability_zone       = "ap-south-1a"
+  cidr_block              = element(var.public-subnets, count.index)
+  availability_zone       = element(var.azs,count.index)
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "Public Subnet 1"
+    Name = "${var.envname}-pubsubnet-${count.index+1}"
   }
 }
 
-# Create Public Subnet 2
-# terraform aws create subnet
-resource "aws_subnet" "public-subnet-2" {
+#Create Private Subnet 
+#Private Subnets using terraform
+resource "aws_subnet" "private-subnets" {
+  count = length(var.azs)
   vpc_id                  = aws_vpc.terraform-vpc.id
-  cidr_block              = var.public-subnet-2-cidr
-  availability_zone       = "ap-south-1b"
-  map_public_ip_on_launch = true
+  cidr_block              = element(var.private-subnets, count.index)
+  availability_zone       = element(var.azs,count.index)
+  map_public_ip_on_launch = false
 
   tags = {
-    Name = "Public Subnet2"
+    Name = "${var.envname}-private-subnets-${count.index+1}"
   }
 }
-# Create Route Table and Add Public Route
-# terraform aws create route table
+
+#Create Data Subnet 
+#Data Subnets using terraform
+resource "aws_subnet" "data-subnets" {
+  count = length(var.azs)
+  vpc_id                  = aws_vpc.terraform-vpc.id
+  cidr_block              = element(var.data-subnets, count.index)
+  availability_zone       = element(var.azs,count.index)
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "${var.envname}-data-subnet-${count.index+1}"
+  }
+}
+#EIP
+resource "aws_eip" "natIp" {
+    vpc      = true
+  tags = {
+    Name = "${var.envname}-natIp"
+  }
+}
+# Create Nat Gateway
+resource "aws_nat_gateway" "NatGw" {
+  allocation_id = aws_eip.natIp.id
+  subnet_id     = aws_subnet.public-subnets[0].id
+
+  tags = {
+    Name = "${var.envname}-natGW"
+  }
+}
+
+# Create a Public Route Table
 resource "aws_route_table" "public-route-table" {
   vpc_id = aws_vpc.terraform-vpc.id
 
@@ -54,114 +86,52 @@ resource "aws_route_table" "public-route-table" {
   }
 
   tags = {
-    Name = "Public route Table"
+    Name = "${var.envname}-publicroute"
   }
 }
-# Associate Public Subnet 1 to "Public Route Table"
-# terraform aws associate subnet with route table
-resource "aws_route_table_association" "public-subnet-1-route-table-association" {
-  subnet_id      = aws_subnet.public-subnet-1.id
-  route_table_id = aws_route_table.public-route-table.id
-}
-# Associate Public Subnet 2 to "Public Route Table"
-# terraform aws associate subnet with route table
-resource "aws_route_table_association" "public-subnet-2-route-table-association" {
-  subnet_id      = aws_subnet.public-subnet-2.id
-  route_table_id = aws_route_table.public-route-table.id
-}
-
-# Create Private Subnet 1
-# terraform aws create subnet
-resource "aws_subnet" "private-subnet-1" {
-  vpc_id                  = aws_vpc.terraform-vpc.id
-  cidr_block              = var.private-subnet-1-cidr
-  availability_zone       = "ap-south-1a"
-  map_public_ip_on_launch = false
-
-  tags = {
-    Name = "Private Subnet 1"
-  }
-}
-# Create Private Subnet 2
-# terraform aws create subnet
-resource "aws_subnet" "private-subnet-2" {
-  vpc_id                  = aws_vpc.terraform-vpc.id
-  cidr_block              = var.private-subnet-2-cidr
-  availability_zone       = "ap-south-1b"
-  map_public_ip_on_launch = false
-
-  tags = {
-    Name = "Private Subnet 2"
-  }
-}
-# Create Private Subnet 3
-# terraform aws create subnet
-resource "aws_subnet" "private-subnet-3" {
-  vpc_id                  = aws_vpc.terraform-vpc.id
-  cidr_block              = var.private-subnet-3-cidr
-  availability_zone       = "ap-south-1a"
-  map_public_ip_on_launch = false
-
-  tags = {
-    Name = "Private Subnet 3 - DB"
-  }
-}
-# Create Private Subnet 4
-# terraform aws create subnet
-resource "aws_subnet" "private-subnet-4" {
-  vpc_id                  = aws_vpc.terraform-vpc.id
-  cidr_block              = var.private-subnet-4-cidr
-  availability_zone       = "ap-south-1b"
-  map_public_ip_on_launch = false
-
-  tags = {
-    Name = "Private Subnet 4 - DB"
-  }
-}
-#create nat gate way
-#create elp ip allocation nat gate  way
-
-resource "aws_eip" "ip" {
-  vpc = true
-}
-resource "aws_nat_gateway" "ngw" {
-  allocation_id = aws_eip.ip.id
-  subnet_id     = aws_subnet.public-subnet-2.id
-
-  tags = {
-    Name = "NGW"
-  }
-}
+# Create a Private Route Table Association
 
 resource "aws_route_table" "private-route-table" {
   vpc_id = aws_vpc.terraform-vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.ngw.id
+    nat_gateway_id = aws_nat_gateway.NatGw.id
   }
 
   tags = {
-    Name = "private route Table"
+    Name = "${var.envname}-privateroute"
   }
 }
+
+resource "aws_route_table" "date-route-table" {
+  vpc_id = aws_vpc.terraform-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.NatGw.id
+  }
+
+  tags = {
+    Name = "${var.envname}-dataroute"
+  }
+}
+
 # Associate Public Subnet 1 to "Public Route Table"
-# terraform aws associate subnet with route table
-resource "aws_route_table_association" "private-subnet-1-route-table-association" {
-  subnet_id      = aws_subnet.private-subnet-1.id
+resource "aws_route_table_association" "pubrouteassociation" {
+  count = length(var.public-subnets)
+  subnet_id      = element(aws_subnet.public-subnets.*.id,count.index)
+  route_table_id = aws_route_table.public-route-table.id
+}
+
+resource "aws_route_table_association" "privaterouteassociation" {
+  count = length(var.private-subnets)
+  subnet_id      = element(aws_subnet.private-subnets.*.id,count.index)
   route_table_id = aws_route_table.private-route-table.id
 }
-# Associate Public Subnet 2 to "Public Route Table"
-# terraform aws associate subnet with route table
-resource "aws_route_table_association" "private-subnet-2-route-table-association" {
-  subnet_id      = aws_subnet.private-subnet-2.id
-  route_table_id = aws_route_table.private-route-table.id
+
+resource "aws_route_table_association" "datarouteassociation" {
+  count = length(var.private-subnets)
+  subnet_id      = element(aws_subnet.data-subnets.*.id,count.index)
+  route_table_id = aws_route_table.date-route-table.id
 }
-resource "aws_route_table_association" "private-subnet-3-route-table-association" {
-  subnet_id      = aws_subnet.private-subnet-3.id
-  route_table_id = aws_route_table.private-route-table.id
-}
-resource "aws_route_table_association" "private-subnet-4-route-table-association" {
-  subnet_id      = aws_subnet.private-subnet-4.id
-  route_table_id = aws_route_table.private-route-table.id
-}  
