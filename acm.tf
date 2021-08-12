63e22e11da7e47896c87a29759a6224e220d71df
Bhaskar-Sadipiralla <86282626+Bhaskar-Sadipiralla@users.noreply.github.com>
2021-08-12 13:41:15 +0530

    Delete acm.tf

--- acm.tf
+++
@@ -1,8 +0,0 @@
-# resource "aws_acm_certificate" "venkat" {
-#   domain_name       = "venkatcloud.xyz"
-#   validation_method = "EMAIL"
-# }
-
-# resource "aws_acm_certificate_validation" "venkat" {
-#   certificate_arn = aws_acm_certificate.venkat.arn
-# }
